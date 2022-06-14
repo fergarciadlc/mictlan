8a56ee289f6f326bf2cc1e7cbd372d61b41b2020
Fernando Garcia <ferchoget@gmail.com>
2022-06-13 23:28:57 -0500

    knob and bg

--- Source/Knob.cpp
+++ Source/Knob.cpp
@@ -13,9 +13,9 @@
 Knob_LookAndFeel::Knob_LookAndFeel()
 {
     // ROTAR IMAGEN
-    knobImage = juce::ImageCache::getFromMemory(BinaryData::knobThree_png, BinaryData::knobThree_pngSize);
+    //knobImage = juce::ImageCache::getFromMemory(BinaryData::knobThree_png, BinaryData::knobThree_pngSize);
     // FILM STRIP
-    //knobImage = juce::ImageCache::getFromMemory(BinaryData::knobTwo_png, BinaryData::knobTwo_pngSize);
+    knobImage = juce::ImageCache::getFromMemory(BinaryData::knob_alpha_png, BinaryData::knob_alpha_pngSize);
 }
 
 Knob_LookAndFeel::~Knob_LookAndFeel(){
@@ -29,42 +29,42 @@ void Knob_LookAndFeel::drawRotarySlider(juce::Graphics& g,
                             float rotaryEndAngle,
                             juce::Slider& slider)
 {
-    //// FILM STRIP--------------------------------------------
-    //const int numFrames = knobImage.getHeight() / knobImage.getWidth();
-    //const int frameIndex = (int)std::ceil(sliderPosProportional * (numFrames - 1));
-    //
-    //const float radius = fmin(width*0.5, height*0.5);
-    //const float centreX = x + width * 0.5f;
-    //const float centreY = y + height * 0.5f;
-    //const float rx = centreX - radius;
-    //const float ry = centreY - radius;
-    //
-    //g.drawImage(knobImage,
-    //            rx,
-    //            ry,
-    //            2 * radius,
-    //            2 * radius,
-    //            0,
-    //            frameIndex * knobImage.getWidth(),
-    //            knobImage.getWidth(),
-    //            knobImage.getWidth());
-    //// FILM STRIP--------------------------------------------
+    // FILM STRIP--------------------------------------------
+    const int numFrames = knobImage.getHeight() / knobImage.getWidth();
+    const int frameIndex = (int)std::ceil(sliderPosProportional * (numFrames - 1));
     
-    // ROTAR IMAGEN -*----------------------------------------------
-    auto radius = (float) juce::jmin (width / 2, height / 2);
-    auto centreX = (float) x + (float) width  * 0.5f;
-    auto centreY = (float) y + (float) height * 0.5f;
-    auto rx = centreX - radius;
-    auto ry = centreY - radius;
+    const float radius = fmin(width*0.5, height*0.5);
+    const float centreX = x + width * 0.5f;
+    const float centreY = y + height * 0.5f;
+    const float rx = centreX - radius;
+    const float ry = centreY - radius;
     
-    //Rotating knob image equation
-    float stopValue = 0.75f;
-    float rotation = stopValue * juce::MathConstants<float>::twoPi * (sliderPosProportional - 0.5f);
-    knobImage = knobImage.rescaled((int)radius * 2,
-                                             (int)radius * 2,
-                                             juce::Graphics::ResamplingQuality::highResamplingQuality);
-    g.drawImageTransformed(knobImage, juce::AffineTransform::rotation(rotation,
-                                                                      static_cast<float>(knobImage.getWidth() / 2),
-                                                                      static_cast<float>(knobImage.getHeight() / 2)).translated(rx, ry));
-    // ROTAR IMAGEN -*----------------------------------------------
+    g.drawImage(knobImage,
+                rx,
+                ry,
+                2 * radius,
+                2 * radius,
+                0,
+                frameIndex * knobImage.getWidth(),
+                knobImage.getWidth(),
+                knobImage.getWidth());
+    // FILM STRIP--------------------------------------------
+    
+    //// ROTAR IMAGEN -*----------------------------------------------
+    //auto radius = (float) juce::jmin (width / 2, height / 2);
+    //auto centreX = (float) x + (float) width  * 0.5f;
+    //auto centreY = (float) y + (float) height * 0.5f;
+    //auto rx = centreX - radius;
+    //auto ry = centreY - radius;
+    //
+    ////Rotating knob image equation
+    //float stopValue = 0.75f;
+    //float rotation = stopValue * juce::MathConstants<float>::twoPi * (sliderPosProportional - 0.5f);
+    //knobImage = knobImage.rescaled((int)radius * 2,
+    //                                         (int)radius * 2,
+    //                                         juce::Graphics::ResamplingQuality::highResamplingQuality);
+    //g.drawImageTransformed(knobImage, juce::AffineTransform::rotation(rotation,
+    //                                                                  static_cast<float>(knobImage.getWidth() / 2),
+    //                                                                  static_cast<float>(knobImage.getHeight() / 2)).translated(rx, ry));
+    //// ROTAR IMAGEN -*----------------------------------------------
 }
--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -18,7 +18,7 @@ MictlanAudioProcessorEditor::MictlanAudioProcessorEditor (MictlanAudioProcessor&
     slider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, false, 0, 0);
     slider.setLookAndFeel(&lookAndFeel);
     addAndMakeVisible(slider);
-    setSize (500, 250);
+    setSize (250, 250);
 }
 
 MictlanAudioProcessorEditor::~MictlanAudioProcessorEditor()
@@ -32,16 +32,17 @@ void MictlanAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
     g.fillAll(juce::Colours::black);
-    g.drawImageAt(juce::ImageCache::getFromMemory(BinaryData::bg1_png, BinaryData::bg1_pngSize), 250, 0);
-    //g.drawImageAt(juce::ImageCache::getFromMemory(BinaryData::bg2_png, BinaryData::bg2_pngSize), 0, 0);
+    g.drawImageAt(juce::ImageCache::getFromMemory(BinaryData::bg_png, BinaryData::bg_pngSize), 0, 0);
+    //g.drawImageAt(juce::ImageCache::getFromMemory(BinaryData::bg1_png, BinaryData::bg1_pngSize), 0, 0);
+    //g.drawImageAt(juce::ImageCache::getFromMemory(BinaryData::bg3_png, BinaryData::bg3_pngSize), 0, 0);
 
     g.setColour (juce::Colours::white);
     g.setFont (20.0f);
-    g.drawFittedText ("MICTLAN", getLocalBounds(), juce::Justification::centredBottom, 1);
+    //g.drawFittedText ("MICTLAN", getLocalBounds(), juce::Justification::centredBottom, 1);
 }
 
 void MictlanAudioProcessorEditor::resized()
 {
-    float bigDiameter = 0.6f;
-    slider.setBoundsRelative(0.25f - bigDiameter / 2, 0.5f - bigDiameter / 2, bigDiameter, bigDiameter);
+    float bigDiameter = 0.52f;
+    slider.setBoundsRelative(0.5f - bigDiameter / 2, 0.5f - bigDiameter / 2, bigDiameter, bigDiameter);
 }
